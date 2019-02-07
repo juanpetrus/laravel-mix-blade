4f76b3ac1ac457cfa81e21334a0b91974fdf16b9
Juan Petrus <juan.petruss@gmail.com>
2019-02-07 12:38:19 -0300

    Configurando css com mix

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.combine([
+  'resources/assets/js/jquery.js',
+  'resources/assets/js/materialize.js',
+], 'public/js/app.js');
+
+mix.combine([
+  'resources/assets/css/materialize.css',
+  'resources/assets/css/style.css'
+],'public/css/app.css');
